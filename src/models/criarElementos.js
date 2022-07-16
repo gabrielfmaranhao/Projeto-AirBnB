8cf0066751c401b2e4e95f2554370a6970502e13
Gabriel Maranhão <gabrielmaranhao48@gmail.com>
2022-07-16 16:39:27 -0300

    Adicionando o css do header

--- src/models/criarElementos.js
+++ src/models/criarElementos.js
@@ -4,13 +4,19 @@ export default class CriarElementos {
         const header            = document.createElement("header");
         const img               = document.createElement("img");
         img.src                 = "/logos/logo.png";
-        img.alt                 = "Logo Airbnb"
+        img.alt                 = "Logo Airbnb";
+        img.id                  = "imgLogo";
         const div               = document.createElement("div");
         const button            = document.createElement("button");
         button.innerText        = "Location";
+        button.classList.add("buttonHeader");
+        button.id               = "buttonLocation";
         const buttonGuests      = document.createElement("button");
         buttonGuests.innerText  = "Add guest";
+        buttonGuests.classList.add("buttonHeader");
+        buttonGuests.id         = "buttonGuests";
         const search            = document.createElement("button");
+        search.classList.add("buttonHeader");
         const imgSearch         = document.createElement("img");
         imgSearch.src           = "/logos/vecteezy_magnifying-glass-icon-vector-on-white-background-simple-logo_.jpg";
         imgSearch.alt           = "Logo search";
